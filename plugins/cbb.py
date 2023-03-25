e5558cbe746a0b47ea915b00727a38b92b564c37
narutokilluaa <127219032+narutokilluaa@users.noreply.github.com>
2023-03-25 15:48:59 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,7 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>This Person</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Source Code : <a href='https://github.com/CodeXBotz/File-Sharing-Bot'>Click here</a>\n○ Channel : @CodeXBotz\n○ Support Group : @CodeXBotzSupport</b>",
+            text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>This Person</a>\n○ Language : <code>Dual</code>\n○ Library : <a href='https://t.me/anime_ongoing_23'>Here</a>\n○ Request New : <a href='https://t.me/newanimerequestbot'>Click here</a>\n○ Channel : @anime_ongoing_23\n○ Chat Group : @zenitsupub</b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
